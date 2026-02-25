a9770687384eab96076e51f541ecd738139d1546
JohnZhu9122 <zjw2088073480@163.com>
2026-02-24 18:34:05 -0800

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-#hello.py
-#Jiawei Zhu
-#
-#Using the way of input that to instead of the variable
-#name is the variable to be enter
-name = input("What's your name?")
-print(f"Hi {name}")               
