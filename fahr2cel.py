32285745a13aa9d49c49af26f9cb99011bef323c
JohnZhu9122 <zjw2088073480@163.com>
2026-02-24 18:32:00 -0800

    Delete fahr2cel.py

--- fahr2cel.py
+++
@@ -1,7 +0,0 @@
-#fahr2cel.py
-#Jiawei Zhu
-#
-#Convert degrees Fahrenheit to Celsius
-fahrenheit = 32
-celsius = (fahrenheit - 32.0)*(5/9) #covert the unit of fahrenheit to celsius
-print(fahrenheit,"Fahrenheit is", celsius, "Celsius.")
