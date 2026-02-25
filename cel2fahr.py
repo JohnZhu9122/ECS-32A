7c7a0bab3d24e2e40e47ba5634fae41bdad99f3f
JohnZhu9122 <zjw2088073480@163.com>
2026-02-24 18:32:13 -0800

    Delete cel2fahr.py

--- cel2fahr.py
+++
@@ -1,7 +0,0 @@
-#cel2fahr.py
-#Jiawei Zhu
-#
-#Converge the Fahrenheit to Celsius with the exactuly value it wants.
-fahrenheit = 89.6
-celsius = 32
-print (f"{celsius} Celsius is {fahrenheit} Fahrenheit.")
